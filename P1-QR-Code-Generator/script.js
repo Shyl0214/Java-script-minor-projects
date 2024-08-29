095f04f80c6cdd207aa01fdcad2402796fdb2247
Shailesh Kumar <shaileshkumarsingh988@gmail.com>
2024-08-29 21:52:04 +0530

    made structure

--- P1-QR-Code-Generator/script.js
+++ P1-QR-Code-Generator/script.js
@@ -1,3 +1,19 @@
-let h1 = document.querySelector("h1");
+let inp = document.querySelector("#inp");
+let btn = document.querySelector("#btn");
+let img = document.querySelector("img");
 
-console.log(h1);
+let inputValue;
+
+btn.addEventListener("click", function () {
+  inputValue = inp.value;
+  console.log(inputValue);
+
+  if (inp.value === "") {
+    alert("Please first fill the input box");
+  } else {
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data= ${inputValue}`;
+
+    img.alt = `QR Code for ${inputValue}`;
+    console.log(img.alt);
+  }
+});
