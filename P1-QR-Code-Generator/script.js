4d79729937614b4e8cbafc437c1d40fe0a2110b7
Shailesh Kumar <shaileshkumarsingh988@gmail.com>
2024-08-29 22:06:02 +0530

    modified the js logic

--- P1-QR-Code-Generator/script.js
+++ P1-QR-Code-Generator/script.js
@@ -1,19 +1,22 @@
 let inp = document.querySelector("#inp");
 let btn = document.querySelector("#btn");
 let img = document.querySelector("img");
+let h4 = document.querySelector("h4");
 
-let inputValue;
+let inpValue;
 
 btn.addEventListener("click", function () {
-  inputValue = inp.value;
-  console.log(inputValue);
+  inpValue = inp.value;
+  //   console.log(inpValue);
 
-  if (inp.value === "") {
-    alert("Please first fill the input box");
+  if (inpValue === "") {
+    alert("Please fill the input field");
   } else {
-    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data= ${inputValue}`;
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data= ${inpValue}`;
 
-    img.alt = `QR Code for ${inputValue}`;
-    console.log(img.alt);
+    img.alt = `QR code for the link is ${inpValue}`;
+
+    h4.textContent = "Your Qr-code is ready";
+    h4.style.marginTop = "5px";
   }
 });
