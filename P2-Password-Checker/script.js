ffc4cb7f4ab3a36502f577eab8e759af38c335a0
Shailesh Kumar <shaileshkumarsingh988@gmail.com>
2024-08-31 00:15:33 +0530

    working on adding js logic

--- P2-Password-Checker/script.js
+++ P2-Password-Checker/script.js
@@ -4,6 +4,29 @@ let btn = document.querySelector("#btn");
 
 let password;
 
-inp.addEventListener("input", function () {
+btn.addEventListener("click", function () {
   password = inp.value;
+
+  if (!password) {
+    h2.textContent = "Please enter the Password!!";
+    h2.style.color = "red";
+  } else {
+    h2.textContent = "";
+    // length
+    if (password.length < 8) {
+      h2.textContent = "Password is too short";
+    } else if (password.search(/[a-z]/) == -1) {
+      h2.textContent = "Password is missing small case";
+    } else if (password.search(/[A-Z]/) == -1) {
+      h2.textContent = "Password is missing captial case";
+    } else if (password.search(/[]/)) {
+    } else {
+      h2.textContent = "Password is Strong";
+      h2.style.color = "green";
+    }
+  }
+  // too short
+  // lower case
+  // upper case
+  // special character
 });
